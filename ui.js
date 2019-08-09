1983989c89a329c9ad43685495ec91c1858ee26c
Cameron May <camerondmay@gmail.com>
2019-08-10 09:45:56 +1200

    Update ui.js

--- ui.js
+++ ui.js
@@ -6,23 +6,26 @@ function writeTextUI() {
   // button1 = createButton('Restart');
   // button1.position((10 * lmax), windowHeight - lmax * 6);
   button2 = createButton('Next');
-  button2.position(windowWidth - (10 * lmax) - (lmax * 10), windowHeight - lmax * 6);
-  colH2 = color(130, 50, 50);
-  colH1 = color(355, 50, 50);
-
-  // button1.style('background-color', colH1);
-  // button1.style('font-size', '2.1vmax');
-  // button1.style('color', 'white');
-  // button1.style('border-radius', '0.5vmax')
-  // button1.style('width', '14vmax')
-  // button1.mousePressed(restart);
+  button2.position(windowWidth - (20 * lmax) - (lmax * 3), windowHeight - lmax * 5);
+  colH2 = color(130, 70, 80);
+  colH3 = color(240, 70, 80);
+  colH1 = color(355, 70, 80);
 
   button2.style('background-color', colH2);
-  button2.style('font-size', '2.1vmax');
+  button2.style('font-size', '2.5vmax');
   button2.style('color', 'white');
   button2.style('border-radius', '0.5vmax')
-  button2.style('width', '14vmax')
+  button2.style('width', '20vmax')
   button2.mousePressed(nextGrid);
+
+  button4 = createButton('Save');
+button4.position(windowWidth - (20 * lmax) - (lmax * 3), windowHeight - lmax * 10.5);
+button4.style('background-color', colH3);
+button4.style('font-size', '2.5vmax');
+button4.style('color', 'white');
+button4.style('border-radius', '0.25vmax')
+button4.style('width', '20vmax')
+button4.mousePressed(saveImage);
 
   }
 
@@ -35,14 +38,13 @@ function writeTextUI() {
 
     button2.remove();
     button2 = createButton('Start Over');
-    button2.position(windowWidth - (10 * lmax) - (lmax * 10), windowHeight - lmax * 6);
-    colH2 = color(10, 80, 50);
-    colH1 = color(355, 50, 50);
+    button2.position(windowWidth - (20 * lmax) - (lmax * 3), windowHeight - lmax * 5);
+    colH2 = color(355, 70, 80);
     button2.style('background-color', colH2);
-    button2.style('font-size', '2.1vmax');
+    button2.style('font-size', '2.5vmax');
     button2.style('color', 'white');
     button2.style('border-radius', '0.5vmax')
-    button2.style('width', '14vmax')
+    button2.style('width', '20vmax')
     button2.mousePressed(restart);
 
     }
@@ -53,4 +55,8 @@ function writeTextUI() {
         stage = 0;
     setup();
 
+  }
+
+  function saveImage(){
+    save('dreamscape'+month()+day()+hour()+second()+'.jpg');
   }
